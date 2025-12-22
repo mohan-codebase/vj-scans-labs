6c880f1c869facc7154027d11f1be81afe7d605f
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-12-22 18:31:04 +0530

    Fix : New Bugs fixed

--- src/components/features/specialities/onco-diagnostics/Section1.tsx
+++ src/components/features/specialities/onco-diagnostics/Section1.tsx
@@ -9,7 +9,7 @@ const slides = [
         id: 1,
         title: <span className='font-sans font-[650] text-[#0961A1]'>Onco <span className='text-[#F98D1B]'>Diagnostics</span></span>,
         subtitle: <span className="text-[#1E1E1E] font-secondary font-medium">
-our Onco Diagnostics division is dedicated to the early detection, accurate diagnosis, and monitoring of cancer through advanced laboratory testing. We combine cutting-edge technology, expert pathology review, and stringent quality standards to support clinicians in making confident, timely treatment decisions.
+Our Onco Diagnostics division is dedicated to the early detection, accurate diagnosis, and monitoring of cancer through advanced laboratory testing. We combine cutting-edge technology, expert pathology review, and stringent quality standards to support clinicians in making confident, timely treatment decisions.
 </span>,
         image: "/images/Section1/banner-img-1-by-vj.avif",
         mobileImage: "/images/Section1/mobileBanner-img-1-by-vj.avif",
