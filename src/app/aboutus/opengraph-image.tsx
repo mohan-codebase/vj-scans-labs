51fd4c09c930129ff622d662c70eed33022eb632
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2026-02-05 17:31:30 +0530

    Update opengraph-image.tsx

--- src/app/aboutus/opengraph-image.tsx
+++ src/app/aboutus/opengraph-image.tsx
@@ -4,6 +4,7 @@ export const runtime = 'edge';
 export const alt = 'About VJ Scans & Labs';
 export const contentType = 'image/png';
 
+// Force cache bust
 export default async function Image() {
     return OgImageGenerator({
         title: 'About VJ Scans',
